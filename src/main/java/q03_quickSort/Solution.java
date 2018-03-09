cd20193b98d969b0f996b09f95907be1fba87e98
mengrLi <34010243+mengrLi@users.noreply.github.com>
2018-03-09 16:20:30 -0500

    Update Solution.java

--- src/main/java/q03_quickSort/Solution.java
+++ src/main/java/q03_quickSort/Solution.java
@@ -2,6 +2,54 @@ package q03_quickSort;
 
 public class Solution {
     public void quickSort(int[] arr) {
+        quickSortRE(arr,0,arr.length-1);
+
 
     }
+    private void quickSortRE(int arr[],int low,int high)
+    {
+        if (arr == null || arr.length == 0)
+            return ;
+
+        if (low >= high)
+            return ;
+
+        // pick the pivot
+        int middle = low + (high - low) / 2;
+        int pivot = arr[middle];
+
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+
+        if (low < j)
+            quickSortRE(arr, low, j);
+
+        if (high > i)
+            quickSortRE(arr, i, high);
+
+
+
+    }
+
+
+
+
 }
